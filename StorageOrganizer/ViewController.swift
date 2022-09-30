bd9db02bb9cacc912f13799443e5340a9e28330f
Aaron Cleveland <toscleveland@gmail.com>
2022-09-29 22:02:27 -0500

    Initial Commit

---
+++ StorageOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StorageOrganizer
+//
+//  Created by Aaron Cleveland on 9/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+     }
+
+
+}
+
